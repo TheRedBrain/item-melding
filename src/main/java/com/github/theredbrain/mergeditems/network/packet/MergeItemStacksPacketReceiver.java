60ff543ad8768532f3bfd023d30930247f53673b
TheRedBrain <thetrueredbrain@gmail.com>
2024-11-02 19:24:04 +0100

    new stuff
    -added ItemMergingUtilityComponent
    -added mod icon

--- src/main/java/com/github/theredbrain/mergeditems/network/packet/MergeItemStacksPacketReceiver.java
+++ src/main/java/com/github/theredbrain/mergeditems/network/packet/MergeItemStacksPacketReceiver.java
@@ -1,11 +1,13 @@
 package com.github.theredbrain.mergeditems.network.packet;
 
+import com.github.theredbrain.mergeditems.component.type.ItemMergingUtilityComponent;
 import com.github.theredbrain.mergeditems.component.type.MergedItemsComponent;
 import com.github.theredbrain.mergeditems.registry.ItemComponentRegistry;
 import com.github.theredbrain.mergeditems.screen.ItemMergingScreenHandler;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.TagKey;
 import net.minecraft.screen.ScreenHandler;
@@ -25,7 +27,7 @@ public class MergeItemStacksPacketReceiver implements ServerPlayNetworking.PlayP
 
 		if (screenHandler instanceof ItemMergingScreenHandler itemMergingScreenHandler) {
 			ItemStack containerItemStack = itemMergingScreenHandler.inventory.getStack(1);
-			ItemStack meldedItemStack = itemMergingScreenHandler.inventory.getStack(0);
+			ItemStack mergedItemStack = itemMergingScreenHandler.inventory.getStack(0);
 
 			List<Identifier> identifierList = itemMergingScreenHandler.getMergableItemTags();
 
@@ -39,12 +41,12 @@ public class MergeItemStacksPacketReceiver implements ServerPlayNetworking.PlayP
 				return;
 			}
 
-			if (meldedItemStack.getMaxCount() > 1) {
+			if (mergedItemStack.getMaxCount() > 1) {
 				player.sendMessage(Text.translatable("hud.message.item_merging.item_can_not_be_merged_into_other_items"));
 				return;
 			}
 
-			MergedItemsComponent mergedItemsComponentOfMeldedItemStack = meldedItemStack.get(ItemComponentRegistry.MERGED_ITEMS_COMPONENT_TYPE);
+			MergedItemsComponent mergedItemsComponentOfMeldedItemStack = mergedItemStack.get(ItemComponentRegistry.MERGED_ITEMS_COMPONENT_TYPE);
 
 			if (mergedItemsComponentOfMeldedItemStack != null && !mergedItemsComponentOfMeldedItemStack.isEmpty()) {
 				player.sendMessage(Text.translatable("hud.message.item_merging.merged_item_stack_has_items_merged"));
@@ -55,24 +57,24 @@ public class MergeItemStacksPacketReceiver implements ServerPlayNetworking.PlayP
 			if (mergedItemsComponentOfContainerItemStack == null) {
 				player.sendMessage(Text.translatable("hud.message.item_merging.item_can_not_be_merged_into", containerItemStack.getName()));
 			} else {
-//				MergedItemsComponent containerStackMergedItemsComponent = containerItemStack.getOrDefault(ItemComponentRegistry.MELDING_COMPONENT_TYPE, MergedItemsComponent.DEFAULT);
-//				List<ItemStack> meldedItemStacksOfContainerItemStack = containerStackMergedItemsComponent.melded_items();
 
 				if (mergedItemsComponentOfContainerItemStack.size() >= itemMergingScreenHandler.getMaxMergedItemsAmount()) {
 					player.sendMessage(Text.translatable("hud.message.item_merging.item_has_reached_max_amount_of_merged_items", containerItemStack.getName()));
 					return;
 				}
 
+				ItemMergingUtilityComponent itemMergingUtilityComponent = containerItemStack.get(ItemComponentRegistry.ITEM_MERGING_UTILITY_COMPONENT_TYPE);
+				if (itemMergingUtilityComponent != null) {
+					String possible_merging_items = itemMergingUtilityComponent.possible_merging_items();
+					if (!possible_merging_items.isEmpty() && !mergedItemStack.isIn(TagKey.of(Registries.ITEM.getKey(), Identifier.of(possible_merging_items)))) {
+						player.sendMessage(Text.translatable("hud.message.item_merging.item_can_not_be_merged_into_item", mergedItemStack.getName(), containerItemStack.getName()));
+						return;
+					}
+				}
+
 				MergedItemsComponent.Builder builder = new MergedItemsComponent.Builder(mergedItemsComponentOfContainerItemStack);
-//			String possible_melded_items = containerStackMergedItemsComponent.possible_melded_items();
-//			if (!Objects.equals(possible_melded_items, "") && !meldedItemStack.isIn(TagKey.of(Registries.ITEM.getKey(), Identifier.of(possible_melded_items)))) {
-//				player.sendMessage(Text.translatable("hud.message.item_merging.item_can_not_be_melded_into_item"));
-//				return;
-//			}
-				builder.add(meldedItemStack);
-//				containerStackMergedItemsComponent.melded_items().add(meldedItemStack.copy());
+				builder.add(mergedItemStack);
 				containerItemStack.set(ItemComponentRegistry.MERGED_ITEMS_COMPONENT_TYPE, builder.build());
-//				containerItemStack.applyChanges(ComponentChanges.builder().add(ItemComponentRegistry.MELDING_COMPONENT_TYPE, containerStackMergedItemsComponent).build());
 
 				itemMergingScreenHandler.inventory.setStack(0, ItemStack.EMPTY);
 			}
--- src/main/java/com/github/theredbrain/mergeditems/registry/ItemComponentRegistry.java
+++ src/main/java/com/github/theredbrain/mergeditems/registry/ItemComponentRegistry.java
@@ -1,6 +1,7 @@
 package com.github.theredbrain.mergeditems.registry;
 
 import com.github.theredbrain.mergeditems.MergedItems;
+import com.github.theredbrain.mergeditems.component.type.ItemMergingUtilityComponent;
 import com.github.theredbrain.mergeditems.component.type.MergedItemsComponent;
 import net.minecraft.component.ComponentType;
 import net.minecraft.registry.Registries;
@@ -11,6 +12,11 @@ public class ItemComponentRegistry {
 			Registries.DATA_COMPONENT_TYPE,
 			MergedItems.identifier("merged_items"),
 			ComponentType.<MergedItemsComponent>builder().codec(MergedItemsComponent.CODEC).packetCodec(MergedItemsComponent.PACKET_CODEC).build()
+	);
+	public static final ComponentType<ItemMergingUtilityComponent> ITEM_MERGING_UTILITY_COMPONENT_TYPE = Registry.register(
+			Registries.DATA_COMPONENT_TYPE,
+			MergedItems.identifier("item_merging_utility"),
+			ComponentType.<ItemMergingUtilityComponent>builder().codec(ItemMergingUtilityComponent.CODEC).packetCodec(ItemMergingUtilityComponent.PACKET_CODEC).build()
 	);
 
 	public static void init() {
